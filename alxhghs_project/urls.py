f349e7083b8da9f5de76d1b416304d1ad74680eb
Alex <alxhghs@gmail.com>
2017-05-29 12:01:36 -0400

    added static import to urls.py

--- alxhghs_project/urls.py
+++ alxhghs_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
