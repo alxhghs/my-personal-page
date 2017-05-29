c42e811736c3a750d75333cb5efdb52619a8312c
Alex <alxhghs@gmail.com>
2017-05-29 11:59:31 -0400

    created core.index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from core.models import Project
 
-# Create your views here.
+
+def index(request):
+    title = "Alex Hughes"
+    projects = Project.objects.order_by('-date')
+    context_dict = {
+                    'projects': projects,
+                    'title': title,
+                    }
+    return render(request, 'core/templates/index.html', context_dict)
