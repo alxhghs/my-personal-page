c7b760f775e3de2043ec1c491ac04ab73e5ba4b4
Alex <alxhghs@gmail.com>
2017-05-29 11:39:45 -0400

    first commit

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from versatileimagefield.fields import VersatileImageField
+
+
+class Project(models.Model):
+    img = VersatileImageField('Image', upload_to='project_images/')
+    description = models.TextField()
+
+
+class Resume(models.Model):
+    TYPE_CHOICES = (
+        ('Volunteer', 'Volunteer'),
+        ('Work', 'Work'),
+        ('Education', 'Education'),
+        ('4Winter', 'Winter'),
+    )
+    _max_length = 128
+    _type = models.Charfield(max_length=_max_length, choices=TYPE_CHOICES)
+    title = models.Charfield(max_length=_max_length)
+    organization = models.Charfield(max_length=_max_length)
