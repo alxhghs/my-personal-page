68e413b57c7b16e69c6c0d8cc1bf212566dfa601
Alex <alxhghs@gmail.com>
2017-05-29 11:55:11 -0400

    fixed typo in core.urls

--- alxhghs_project/urls.py
+++ alxhghs_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('corel.urls')),
+    url(r'', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
