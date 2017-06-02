025e7ecb7c8033c1d67c4a3eb2b1b1426630fbdd
Alex <alxhghs@gmail.com>
2017-06-02 06:21:55 -0400

    trying to add smooth scroll to element

---
+++ static/js/alx_hghs.js
@@ -0,0 +1,9 @@
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
