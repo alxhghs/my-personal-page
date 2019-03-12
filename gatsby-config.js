b6c4155bad1f590d46ba33e2651f817765e41abf
Alex Hughes <alxhghs@gmail.com>
2019-03-12 11:46:51 -0400

    make icon blank for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-        icon: ``,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
