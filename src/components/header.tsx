996811fd233880c48d9c1dff4c1eb9fc1afedd27
Alex Hughes <alxhghs@gmail.com>
2019-03-15 16:22:39 -0400

    add rainbow effect to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import { Link } from "../components";
 
 const Animation = keyframes`
     0% {
-        background-position: 10% 0%;
+        background-position: 0% 82%;
     }
     50% {
-        background-position: 91% 100%;
+        background-position: 100% 19%;
     }
     100% {
-        background-position: 10% 0%;
+        background-position: 0% 82%;
     }
 `;
 
@@ -19,10 +19,11 @@ const StyledHeader = styled("header")`
     display: grid;
     grid-template-columns: auto auto;
     gap: 30px;
-    background: linear-gradient(130deg, #000, #119);
-    background-size: 200% 200%;
+    background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
+    background-size: 1800% 1800%;
     justify-content: center;
-    animation: ${Animation} 10s ease infinite;
+    animation: ${Animation} 45s ease infinite;
+    text-shadow: 0 0 3px #000;
 `;
 
 export const Header: React.FC = () => (
