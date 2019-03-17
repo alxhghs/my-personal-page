a142b16350723be8daf84b69f6f0f9dfe46ec94d
Alex Hughes <alxhghs@gmail.com>
2019-03-16 21:26:12 -0400

    add blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,46 @@
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
-
-// exports.onCreateNode = ({ node, getNode }) => {
-//     if (node.internal.type === "ContentfulBlogPost") {
-//         // const fileNode = getNode(node.parent);
-//         // console.log("\n", fileNode.relativePath);
-//         // console.log("fileNode: ", fileNode);
-//         console.log("node: ", node);
-//     }
-// }
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions;
+    return graphql(`
+        {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        title
+                        dateCreated(formatString: "MMMM DD, YYYY")
+                        content {
+                            childContentfulRichText {
+                                html
+                            }
+                        }
+                        hero {
+                            fluid(maxWidth: 300) {
+                                src
+                                base64
+                            }
+                        }
+                        slug
+                    }
+                }
+            }
+        }
+    `
+    ).then(result => {
+        result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+            createPage({
+                path: node.slug,
+                component: path.resolve("./src/templates/blog-post.tsx"),
+                context: {
+                    slug: node.slug,
+                },
+            });
+        });
+    });
+};
---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogPost: React.FC = () => (
+    <div>hello, blog post</div>
+);
+
+export default BlogPost;
