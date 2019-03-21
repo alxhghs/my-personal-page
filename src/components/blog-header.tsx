f20e20e41c780237e1d33269361e3b996cbb7f26
Alex Hughes <alxhghs@gmail.com>
2019-03-21 15:16:46 -0400

    gtm 2 copy

--- src/components/blog-header.tsx
+++ src/components/blog-header.tsx
@@ -15,8 +15,8 @@ type Props = {
 
 export const BlogHeader: React.FC<Props> = ({ title, subtitle, author, date }) => (
     <StyledBlogHeader>
-        <h2>{ title }</h2>
-        <h3>{ subtitle }</h3>
+        <h1>{ title }</h1>
+        <h2>{ subtitle }</h2>
         <ProfileImage height="100px" width="100px" />
         <h3>by { author }</h3>
         <h4>{ date }</h4>
