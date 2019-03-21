ed08f1c025309e8c706c7821e737b7fbc96a97af
Alex Hughes <alxhghs@gmail.com>
2019-03-21 08:45:14 -0400

    attempt to fix border radius bug

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby";
 import Img, { FluidObject } from "gatsby-image";
+import styled from "@emotion/styled";
 
 type ProfilePictureQuery = {
     file: {
@@ -14,6 +15,12 @@ type ProfileImageProps = {
     height: string;
     width: string;
 };
+
+const ImgWrapper = styled("div")`
+    div, img, picture {
+        border-radius: 50%;
+    }
+`;
 
 export const ProfileImage: React.FC<ProfileImageProps> = ({ height, width }) => {
     const { file }: ProfilePictureQuery = useStaticQuery(
@@ -32,15 +39,17 @@ export const ProfileImage: React.FC<ProfileImageProps> = ({ height, width }) => 
     );
 
     return (
-        <Img
-            fluid={file.childImageSharp.fluid}
-            css={{
-                borderRadius: "50%",
-                height,
-                width,
-                justifySelf: "center",
-                margin: "0 auto",
-            }}
-        />
+        <ImgWrapper>
+            <Img
+                fluid={file.childImageSharp.fluid}
+                css={{
+                    borderRadius: "50%",
+                    height,
+                    width,
+                    justifySelf: "center",
+                    margin: "0 auto",
+                }}
+            />
+        </ImgWrapper>
     );
 };
