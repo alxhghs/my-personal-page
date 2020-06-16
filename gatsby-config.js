333ebb7e40c93fb6b217de3ba1332498e2142acf
alexhughesef <alex.hughes@ef.com>
2020-06-15 20:09:04 -0400

    remove google

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,43 +77,43 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-136725512-1",
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "GTM-TJHWMNV",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10,
-        // cookieDomain: "example.com",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
-      options: {
-        id: "GTM-5HP7DJ6",
-  
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: true,
-  
-        // Specify optional GTM environment details.
-        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
-        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
-      },
-    },
-  ],
+  //   {
+  //     resolve: `gatsby-plugin-google-analytics`,
+  //     options: {
+  //       trackingId: "UA-136725512-1",
+  //       // Puts tracking script in the head instead of the body
+  //       head: true,
+  //       // Setting this parameter is optional
+  //       // anonymize: true,
+  //       // Setting this parameter is also optional
+  //       // respectDNT: true,
+  //       // Avoids sending pageview hits from custom paths
+  //       // exclude: ["/preview/**", "/do-not-track/me/too/"],
+  //       // Enables Google Optimize using your container Id
+  //       optimizeId: "GTM-TJHWMNV",
+  //       // Enables Google Optimize Experiment ID
+  //       // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+  //       // Set Variation ID. 0 for original 1,2,3....
+  //       // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+  //       // Any additional create only fields (optional)
+  //       // sampleRate: 5,
+  //       // siteSpeedSampleRate: 10,
+  //       // cookieDomain: "example.com",
+  //     },
+  //   },
+  //   {
+  //     resolve: `gatsby-plugin-google-tagmanager`,
+  //     options: {
+  //       id: "GTM-5HP7DJ6",
+
+  //       // Include GTM in development.
+  //       // Defaults to false meaning GTM will only be loaded in production.
+  //       includeInDevelopment: true,
+
+  //       // Specify optional GTM environment details.
+  //       // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+  //       // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+  //     },
+  //   },
+  // ],
 }
