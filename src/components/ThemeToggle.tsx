ebd4c036d097b0acd9019829e072a392d74a05d6
Alex Hughes <alxhghs@gmail.com>
2021-02-07 10:14:54 -0500

    add useEffect

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { useTheme } from "../theme";
 import Switch from "react-switch";
@@ -15,6 +15,14 @@ const IconWrapper = styled.div({
 
 export const ThemeToggle: React.FC = () => {
     const { theme, toggleTheme, colors, breakpoints } = useTheme();
+    const [checked, setChecked] = useState(theme === Theme.DARK);
+    useEffect(() => {
+        if (theme === Theme.DARK) {
+            setChecked(true);
+        } else {
+            setChecked(false);
+        }
+    }, [theme]);
     console.log("theme in toggle", theme);
     return (
         <div
@@ -33,7 +41,7 @@ export const ThemeToggle: React.FC = () => {
             }}
         >
             <Switch
-                checked={theme === Theme.DARK}
+                checked={checked}
                 onChange={toggleTheme}
                 color="default"
                 checkedIcon={
