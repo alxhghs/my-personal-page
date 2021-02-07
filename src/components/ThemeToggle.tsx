48af862e0107c1cbfcb93f72f943beb0271b77b0
Alex Hughes <alxhghs@gmail.com>
2021-02-07 00:38:22 -0500

    use material design switch instead

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,21 +1,23 @@
 import { useTheme } from "../theme";
-import DarkModeToggle from "react-dark-mode-toggle";
+import Switch from "@material-ui/core/Switch";
 import { Theme } from "../theme/useUserBrowserTheme";
+import { MdWbSunny } from "react-icons/md";
+import { FiMoon } from "react-icons/fi";
+import React from "react";
 
 export const ThemeToggle: React.FC = () => {
-    const { theme, toggleTheme, colors, breakpoints } = useTheme();
+    const { theme, toggleTheme, colors, breakpoints, setTheme } = useTheme();
     return (
-        <DarkModeToggle
-            size={50}
-            checked={theme === Theme.DARK}
-            onChange={toggleTheme}
-            speed={2.5}
+        <div
             css={{
                 position: "absolute",
                 top: "50%",
                 transform: "translateY(-50%)",
                 right: 8,
                 border: colors.cardBorder,
+                display: "flex",
+                alignItems: "center",
+                "&:hover": { cursor: "pointer" },
                 [`@media screen and (min-width: ${breakpoints[3]})`]: {
                     right: 16,
                 },
@@ -23,6 +25,14 @@ export const ThemeToggle: React.FC = () => {
                     right: 32,
                 },
             }}
-        />
+        >
+            <MdWbSunny onClick={() => setTheme(Theme.LIGHT)} />
+            <Switch
+                checked={theme === Theme.DARK}
+                onChange={toggleTheme}
+                color="default"
+            />
+            <FiMoon onClick={() => setTheme(Theme.DARK)} />
+        </div>
     );
 };
